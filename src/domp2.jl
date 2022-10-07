8096fbedcb7a7405fba6814f7b4c9d4429270204
Claudio Contardo <ccontard@gmail.com>
2022-10-07 08:57:03 -0400

    adding missing files

---
+++ src/domp2.jl
@@ -0,0 +1,44 @@
+using JuMP, MathOptInterface, CPLEX
+
+function domp2(data::DOMPData; time_limit = 7200)::Tuple{Float64, Int64, Vector{Int64}}
+    d = sort(unique(data.D))
+    if d[1] != 0
+        pushfirst!(d, 0)
+    end
+    nd = length(d)
+    nrows = size(data.D, 1)
+    ncols = size(data.D, 2)
+    m = JuMP.direct_model(CPLEX.Optimizer())
+    set_optimizer_attributes(m, 
+        # "CPXPARAM_ScreenOutput" => 0,
+        "CPXPARAM_TimeLimit" => time_limit,
+        "CPXPARAM_Threads" => 1,
+        "CPXPARAM_MIP_Tolerances_MIPGap" => 0.0)
+    @variable(m, x[1 : nrows, 1 : ncols], Bin)
+    @variable(m, y[1 : ncols], Bin)
+    @variable(m, u[1 : nrows, 1 : nd], Bin)
+
+    @objective(m, Min, sum(data.lambda[k] * (d[h] - d[h - 1]) * u[k, h] for k in 1 : nrows, h in 2 : nd))
+
+    @constraint(m, [k in 1 : nrows], u[k, 1] == 0)
+    @constraint(m, [k in 1 : nrows], u[k, nd] == 0)
+
+    @constraint(m, sum(y) == data.p)
+    @constraint(m, [i in 1 : nrows], sum(x[i, :]) == 1)
+    @constraint(m, [i in 1 : nrows, j in 1 : ncols], x[i, j] - y[j] <= 0)
+    @constraint(m, [k in 1 : nrows, h in 2 : nd - 1], u[k, h] - u[k, h + 1] >= 0)
+    @constraint(m, [k in 1 : nrows - 1, h in 2 : nd], u[k + 1, h] - u[k, h] >= 0)
+    @constraint(m, [h in 2 : nd], sum(x[i, j] for i in 1 : nrows for j in 1 : ncols if data.D[i, j] > d[h - 1]) - sum(u[k, h] for k in 1 : nrows) == 0)
+
+    optimize!(m)
+    bb = objective_bound(m)
+    if has_values(m)
+        yval = round.(Int64, value.(y))
+        sol = [j for j in 1 : ncols if yval[j] == 1]
+        dstar = compute_sorted_distances(data, yval)
+        cost = compute_weighted_cost(data, dstar)
+        return bb, cost, sol
+    else
+        return bb, typemax(Int64), Int64[]
+    end
+end
---
+++ src/setpacking.jl
@@ -0,0 +1,127 @@
+using JuMP, CPLEX, MathOptInterface, Graphs
+
+function setpacking(coverage::Matrix{Bool}, k::Int64, p::Int64, supp::Vector{Int64})::Vector{Int64}
+    nrows = size(coverage, 1)
+    ncols = size(coverage, 2)
+    if k < 0 return [] end
+    if p > ncols return [] end
+    if p <= 0 return [1] end
+    result = setpacking_localsearch(coverage, k, p, supp)
+    if isempty(result)
+        result = setpacking_exact(coverage, k, p, supp)
+    end
+    return result
+end
+
+function setpacking_localsearch(coverage::Matrix{Bool}, k::Int64, p::Int64, supp::Vector{Int64})::Vector{Int64}
+    nrows = size(coverage, 1)
+    ncols = size(coverage, 2)
+    cols = collect(1 : ncols)
+    result = Int64[]
+    covered = falses(nrows)
+    newcov = deepcopy(coverage)
+    scov = zeros(Int64, ncols)
+    for i in 1 : p
+        # newcov = Matrix{Bool}(undef, nrows, ncols)
+        for j in cols
+            newcov[:, j] = coverage[:, j] .& .!covered
+            scov[j] = sum(@view newcov[:, j])
+        end
+        sort!(cols; lt = (u, v) -> scov[u] < scov[v] || (scov[u] == scov[v] && supp[u] > supp[v]))
+        jstar = popfirst!(cols)
+        push!(result, jstar)
+        covered = covered .| coverage[:, jstar]
+    end
+    if sum(covered) <= k 
+        # println("feasible heuristic p = $p, k = $k, $result, coverage of $(sum(covered))")
+        return sort(result)
+    else 
+        # println("infeasible heuristic")
+        return Int64[]
+    end
+end
+
+function setpacking_exact(coverage::Matrix{Bool}, k::Int64, p::Int64, supp::Vector{Int64})::Vector{Int64}
+    nrows = size(coverage, 1)
+    ncols = size(coverage, 2)
+    # println("solving setpacking with k = $k, p = $p, nrows = $nrows, ncols = $ncols")
+    # println("cov = $coverage")
+    # println("constructing model")
+    m = JuMP.direct_model(CPLEX.Optimizer())
+    set_optimizer_attributes(m, 
+        # "CPXPARAM_MIP_Tolerances_LowerCutoff" => p - 1e-5, 
+        # "CPXPARAM_MIP_Limits_UpperObjStop" => p - 1e-5,
+        "CPXPARAM_ScreenOutput" => 0,
+        "CPXPARAM_Threads" => 1,
+        "CPXPARAM_MIP_Tolerances_MIPGap" => 0.1,
+        "CPXPARAM_MIP_Limits_Solutions" => 1)
+    conflict = Vector{Vector{Int64}}()
+    let    
+        g = SimpleGraph(ncols)
+        for i in 1 : ncols, j in i + 1 : ncols
+            s = sum(coverage[:, i] .| coverage[:, j])
+            if s > k 
+                add_edge!(g, (i, j))
+            end
+        end
+        edg = collect(edges(g))
+        for e in edg
+            push!(conflict, [src(e), dst(e)])# = Graphs.edges(g)
+        end
+    end
+    subset = Tuple{Int64, Int64}[]
+    equiv = Tuple{Int64, Int64}[]
+    for i in 1 : ncols, j in i + 1 : ncols
+        s = .!coverage[:, i] .| coverage[:, j]
+        t = .!coverage[:, j] .| coverage[:, i]
+        alls = all(s)
+        allt = all(t)
+        if alls && allt
+            push!(equiv, (i, j))
+        elseif alls
+            push!(subset, (i, j)) 
+        elseif allt
+            push!(subset, (j, i))
+        end
+    end
+    
+    # m = JuMP.Model(optimizer_with_attributes(Gurobi.Optimizer, 
+    #     "Cutoff" => p + 1e-5, 
+    #     "BestObjStop" => p + 1e-5,
+    #     "OutputFlag" => 0))
+    # println("hola")
+    @variable(m, x[1 : ncols], Bin)
+    @variable(m, y[1 : nrows], Bin)
+    # println("supp = $supp")
+    @objective(m, Max, dot(supp, x))
+    nonempty_rows = [i for i in 1 : nrows if sum(@view coverage[i, :]) > 0]
+    @constraint(m, [i in nonempty_rows], JuMP.dot(coverage[i, :], x) - min(p, sum(@view coverage[i, :])) * y[i] <= 0)
+    @constraint(m, sum(y) <= k)
+    @constraint(m, sum(x) >= p)
+    if !isempty(conflict)
+        # println("adding $(length(cliques)) clique constraints")
+        @constraint(m, [cl in conflict], sum(x[i] for i in cl) <= 1)
+    end
+    if !isempty(subset)
+        # println("adding $(length(subset)) precedences")
+        @constraint(m, [(i, j) in subset], x[i] - x[j] >= 0)
+    end
+    if false && !isempty(equiv)
+        # println("adding $(length(subset)) precedences")
+        @constraint(m, [(i, j) in equiv], x[i] - x[j] == 0)
+    end
+    optimize!(m)
+    # println(termination_status(m))
+    if termination_status(m) != MOI.INFEASIBLE# && objective_bound(m) >= p - 1e-7
+        # println("feasible setpacking")
+        xval = round.(Int64, value.(x))
+        yval = round.(Int64, value.(y))
+        # println("xval = $([j for j in 1 : ncols if xval[j] == 1])")
+        # println("yval = $([i for i in 1 : nrows if yval[i] == 1])")
+        sol = [i for i in 1 : ncols if xval[i] == 1]
+        return sol[1 : p]
+    else 
+        # println("infeasible setpacking")
+        return Int64[]
+    end
+end
